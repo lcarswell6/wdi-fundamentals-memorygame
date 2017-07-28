42a9be6284d9edc7a081f62d4a2f45bd671efad2
Leon <imacsilver@leon.attlocal.net>
2017-07-27 21:50:12 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
- var cards  = ["queen", "queen", "king", "king"]
+ var cards  = ["queen", "queen", "king", "king"];
  var cardsInPlay = [];
+ 
  var cardOne = cards[0];
- var cardTwo = cards[2];
+ var cardTwo = cards[1];
 
- cardsInPlay.push(cardOne, cardTwo);
-if
-	(cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
-		alert("you have found a match");
-	}else
-	{
-		alert("sorry try again");
+cardsInPlay.push(cardOne, cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("you found a match")
+	}	else {
+		alert("sorry, try again")
 	}
+}
+
 
 console.log();
 console.log("User flipped " + cardOne);
